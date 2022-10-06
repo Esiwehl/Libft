c748f94eb5fd328fe59c00e06acfe7631c4400ac
Eluse Wehl <ewehl@DESKTOP-V3C8S5O.localdomain>
2022-10-07 00:43:20 +0200

    Should be fine now. Added an alternative while, in case main doesn't work...

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,26 +14,37 @@
 #include <stdio.h>
 int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char *cs1 = (unsigned char *) s1;
-	unsigned char *cs2 = (unsigned char *) s2;
+	unsigned char *cs1;
+	unsigned char *cs2;
     size_t idx;
 
     idx = 0;
+    cs1 = (unsigned char *) s1; 
+    cs2 = (unsigned char *) s2;
     while ((cs1[idx] == cs2[idx]) && (idx < n-1))
         idx++;
-	printf("idx = %lu\tn = %lu\n", idx, n);
-	printf("cs1 = %c\tcs2 = %c\n", cs1[idx], cs2[idx]);
     return (cs1[idx] - cs2[idx]);
 }
 
-// #include <string.h>
-// int main()
-// {
-//     char test1[] = "";
-//     char test2[] = "";
+#include <string.h>
+int main()
+{
+    char test1[] = "CHEESE";
+    char test2[] = "CHEESe";
     
-//     printf("O: %d\n", memcmp(test1, test2, sizeof(test1)));
-//     printf("M: %d\n", ft_memcmp(test1, test2, sizeof(test1)));
-//     return (0);
-// }
-// //What do I do if strings aren't |n| long? Nothing?
+    printf("O: %d\n", memcmp(test1, test2, sizeof(test1)));
+    printf("M: %d\n", ft_memcmp(test1, test2, sizeof(test1)));
+    return (0);
+}
+
+/*              ALT WHILE
+    printf("idx = %lu\tn = %lu\n", idx, n);
+	printf("cs1 = %c\tcs2 = %c\n", cs1[idx], cs2[idx]);
+    while(idx < n)
+    {
+        if (cs1[idx] != cs2[idx])
+            return (cs1[idx] - cs2[idx]);
+        idx++;
+    }
+    return (0);
+*/
