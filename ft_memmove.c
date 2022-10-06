4ff93941c897d5295706b308cf610688625667ab
Eluse Wehl <ewehl@DESKTOP-V3C8S5O.localdomain>
2022-10-07 00:45:23 +0200

    This one's been loved. Diff between move and cpy is the check whether it should start at the beginning or end. Also determines speed

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,6 +12,7 @@
 
 // #include "libft.h"
 #include <stdio.h>
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len)
 {
     unsigned char *t_dst;
@@ -19,40 +20,40 @@ void    *ft_memmove(void *dst, const void *src, size_t len)
 
     t_dst = (unsigned char*) dst;
     t_src = (unsigned char*) src;
-    if (!src && !dst)
-		return 0;
+    if (!len)
+		return (dst);
     if (dst <= src)
-        ft_memcpy(t_dst, t_src ,len);
-    else
-        
+        return(ft_memcpy(t_dst, t_src, len));
+	while (len--)
+		*(t_dst + (len)) = *(t_src + (len));
     return (dst);
 }
 
-#include <string.h>
-int	main(void)
-{
-	// char s1[] = "1234";
-	// char s2[] = "9876";
-	int s1[] = {};
-	int s2[] = {};
+// #include <string.h>
+// int	main(void)
+// {
+// 	// char s1[] = "1234";
+// 	// char s2[] = "9876";
+// 	int s1[] = {1,2,3,4};
+// 	int s2[] = {9,8,7,6};
 
-	puts("before");
-	for(size_t i = 0; i < (sizeof(s1)/sizeof(int)); i++)
-	{
-		printf("s2 = %p\t", s2+i);
-		printf("at = %d\t", s2[i]);
-		printf("s1 = %p\t", s1+i);
-		printf("at = %d\n", s1[i]);
-	}
-	// memmove(s2, s1, sizeof(s1));
-	ft_memmove(s2, s1, (sizeof(s1)));
-	puts("\nafter");
-	for(size_t i = 0; i < (sizeof(s1)/sizeof(int)); i++)
-	{
-		printf("s2 = %p\t", s2+i);
-		printf("at = %d\t", s2[i]);
-		printf("s1 = %p\t", s1+i);
-		printf("at = %d\n", s1[i]);
-	}
-    return (0);
-}
+// 	puts("before");
+// 	for(size_t i = 0; i < (sizeof(s1)/sizeof(int)); i++)
+// 	{
+// 		printf("s2 = %p\t", s2+i);
+// 		printf("at = %d\t", s2[i]);
+// 		printf("s1 = %p\t", s1+i);
+// 		printf("at = %d\n", s1[i]);
+// 	}
+// 	// memmove(s2, s1, sizeof(s1));
+// 	ft_memmove(s2, s1, (sizeof(s1)));
+// 	puts("\nafter");
+// 	for(size_t i = 0; i < (sizeof(s1)/sizeof(int)); i++)
+// 	{
+// 		printf("s2 = %p\t", s2+i);
+// 		printf("at = %d\t", s2[i]);
+// 		printf("s1 = %p\t", s1+i);
+// 		printf("at = %d\n", s1[i]);
+// 	}
+//     return (0);
+// }
