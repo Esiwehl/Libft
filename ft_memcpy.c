79e02394026fe7095527c686690ec87afb56e2dc
Eluse Wehl <ewehl@f0r4s10.codam.nl>
2022-10-06 15:55:32 +0200

    Working memcpy, check \if\ i it actually necessary?

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ewehl <ewehl@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/05 17:50:43 by ewehl         #+#    #+#                 */
-/*   Updated: 2022/10/05 19:10:19 by ewehl         ########   odam.nl         */
+/*   Updated: 2022/10/06 15:54:40 by ewehl         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,52 @@
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char *t_dst;
-	const unsigned char *t_src;
+	unsigned char *t_src;
 	size_t idx;
 
 	idx = 0;
 	t_dst = (unsigned char *) dst;
-	t_src = (const unsigned char *) src;
-	if (!src || !dst)
+	t_src = (unsigned char *) src;
+	if (!src && !dst) // Is this necessary thoo
 		return 0;
 	while (idx < n)
 	{
 		t_dst[idx] = t_src[idx];
 		idx++;
 	}
-	return (dst);
+	return (t_dst);
 }
 
 
 // #include <string.h>
 // int	main(void)
 // {
-// 	char s1[] = "";
-// 	char s2[] = "9876";
+// 	// char s1[] = "1234";
+// 	// char s2[] = "9876";
+// 	int s1[] = {9,8,7,6};
+// 	int s2[] = {1,2,3};
 
 // 	puts("before");
-// 	for(size_t i = 0; i < (sizeof(s1)); i++)
+// 	for(size_t i = 0; i < (sizeof(s1)/sizeof(int)); i++)
 // 	{
-// 		printf("%c", s2[i]);
+// 		printf("s2 = %p\t", s2+i);
+// 		printf("at = %d\t", s2[i]);
+// 		printf("s1 = %p\t", s1+i);
+// 		printf("at = %d\n", s1[i]);
 // 	}
-// 	// memcpy(s2, s1, (sizeof(s1)));
-// 	ft_memcpy(s2, s1, (sizeof(s1)));
+// 	memcpy(s2, s1, sizeof(s1));
+// 	// ft_memcpy(s2 + 1, s1, (sizeof(s2)));
 // 	puts("\nafter");
-// 	for(size_t i = 0; i < (sizeof(s1)); i++)
+// 	for(size_t i = 0; i < (sizeof(s1)/sizeof(int)); i++)
 // 	{
-// 		printf("%c", s2[i]);
+// 		printf("s2 = %p\t", s2+i);
+// 		printf("at = %d\t", s2[i]);
+// 		printf("s1 = %p\t", s1+i);
+// 		printf("at = %d\n", s1[i]);
 // 	}
 // }
+
+
+//Pindakaas
+//akaas
+//as
