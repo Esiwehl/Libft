cb29de36d91e842d8f4450f0c5e3d3ef4dc89fdc
Eluse Wehl <eluse.wehl@hva.nl>
2022-10-07 23:15:10 +0200

    Commented out Main, still need to test it

--- ft_strchr.c
+++ ft_strchr.c
@@ -23,14 +23,14 @@ char *ft_strchr(const char *s, int c)
     return (0);
 }
 
-#include <string.h>
-#include <stdio.h>
-int main()
-{
-    char test[] = "Ik ben supercool.";
-    char t = 'd';
+// #include <string.h>
+// #include <stdio.h>
+// int main()
+// {
+//     char test[] = "Ik ben supercool.";
+//     char t = 'd';
     
-    printf("O: %s\n", strchr(test, t));
-    printf("M: %s\n", ft_strchr(test, t));
-    return (0);
-}
+//     printf("O: %s\n", strchr(test, t));
+//     printf("M: %s\n", ft_strchr(test, t));
+//     return (0);
+// }
