8958df2456ab29b1d8bf293e2195a48a4ebe3218
Eluse Wehl <ewehl@f0r6s6.codam.nl>
2022-10-09 20:22:35 +0200

    substr - could be neater, but code seems to work, might wanna look for more edge cases.

---
+++ ft_substr.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_substr.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ewehl <ewehl@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/09 18:26:31 by ewehl         #+#    #+#                 */
+/*   Updated: 2022/10/09 19:28:24 by ewehl         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+size_t ft_strlen(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+    char *substr;
+    size_t idx;
+    size_t pos;
+
+    idx = 0;
+    pos = 0;
+    substr = (char *)malloc((len +1) * sizeof(char));
+    if (!substr || ((size_t)start + len)>= ft_strlen((s)))
+        return (NULL);
+    while (s[idx])
+    {
+        if (idx == (size_t) start)
+        {
+            while (pos <=len)
+            {
+                substr[pos++] = s[idx++];
+                if (pos == len)
+                {
+                    substr[pos] = '\0'; // Is it  even supposed to be null-terminated?!?!
+                    return (substr);
+                }
+            }
+        }
+        idx++;
+    }
+    return (NULL);
+}
+
+// int main()
+// {
+//     char s1[] = "Dit is een moker lange string..";
+//     char *new;
+
+//     new = ft_substr(s1, 30, (size_t) 1);
+//     size_t len = ft_strlen(new);
+//     printf("See if this works:: %s\n", new);
+//     printf("len = %lu", len);
+//     free(new);
+// }
