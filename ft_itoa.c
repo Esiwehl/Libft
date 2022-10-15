96a29c3ab92acb634a79cd720241145aef94fbf5
Eluse Wehl <eluse.wehl@hva.nl>
2022-10-15 21:17:45 +0200

    Should pass francinette now..

--- ft_itoa.c
+++ ft_itoa.c
@@ -37,16 +37,13 @@ char	*ft_itoa(int n)
 	char	*numa;
 	int		len;
 
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
 	len = get_len(&n);
 	numa = (char *)malloc(sizeof(char) * (len + 1));
 	if (!numa)
 		return (NULL);
 	numa[len] = '\0';
-	if (n == -2147483648)
-	{
-		ft_strlcpy(numa, "-2147483648", 12);
-		return (numa);
-	}
 	if (n < 0)
 	{
 		n = -n;
@@ -54,82 +51,19 @@ char	*ft_itoa(int n)
 	}
 	if (n == 0)
 		numa[0] = '0';
-	while (n && len-- > 0)
+	while (len-- > 0 && n)
 	{
 		numa[len] = (n % 10) + '0';
-		if (n > 9)
-			n /= 10;
+		n /= 10;
 	}
 	return (numa);
 }
 
-// static int	amount(int n)
+// int main()
 // {
-// 	int	i;
-
-// 	i = 1;
-// 	if (n < 0)
-// 		i++;
-// 	while (n >= 10 || n <= -10)
-// 	{
-// 		n = n / 10;
-// 		i++;
-// 	}
-// 	return (i);
+// 	int x = 1000;
+// 	char *strx;
+// 	strx = ft_itoa(x);
+// 	printf("strx = %s\n", strx);
+// 	free (strx);
 // }
-
-// static int	edgecasecheck(int n, char *str)
-// {
-// 	if (n == -2147483648)
-// 	{
-// 		ft_strlcpy(str, "-2147483648", 12);
-// 		return (1);
-// 	}
-// 	else if (n == 0)
-// 	{
-// 		str[0] = '0';
-// 		str[1] = '\0';
-// 		return (1);
-// 	}
-// 	return (0);
-// }
-
-// char	*ft_itoa(int n)
-// {
-// 	char	*ret;
-// 	int		i;
-// 	// int		nb;
-
-// 	i = get_len(&n);
-// 	// nb = n;
-// 	if (n < 0)
-// 		n = -n;
-// 	ret = (char *)malloc(i + 1);
-// 	if (ret == NULL)
-// 		return (NULL);
-// 	if (edgecasecheck(n, ret) == 1)
-// 		return (ret);
-// 	ret[i] = '\0';
-// 	// i--;
-// 	while (i-- > 0)
-// 	{
-// 		ret[i] = n % 10 + '0';
-// 		if (n > 9)
-// 			n /= 10;
-// 		// i--;
-// 	}
-// 	if (n < 0)
-// 		ret[0] = '-';
-// 	return (ret);
-// }
-
-int main()
-{
-	int x = -2147483647;
-	char *strx;
-	strx = ft_itoa(x);
-	printf("strx = %s\n", strx);
-	free (strx);
-}
-// check if n == 0
-// check if n = MININT
